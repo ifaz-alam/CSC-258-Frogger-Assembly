869fc95a8979151085375e8ec23c11770b9a4d13
Ifaz A <ifazalam33@gmail.com>
2021-11-28 00:28:28 -0500

    Update froggerNEW.asm
    able to draw rectangle of size x * y in the centre, will soon implement wrap around capability

--- froggerNEW.asm
+++ froggerNEW.asm
@@ -10,6 +10,61 @@ displayAddress: .word 0x10008000
 .text
 lw $t0, displayAddress # $t0 stores the base address for display
 
+j main
+
+DRAW_RECT:
+	# Increment the column position (initially from 0 if new column or brand new call), and draw a pixel
+	addi $t1, $t1, 1
+	sw $a0, 0($t0)
+	
+	# Check if we're done drawing the rectangle
+	beq $t1, $a1, IS_LAST_PIXEL
+	
+	# Shift the displayAddress pivot
+	addi $t0, $t0, 4
+	
+	j DRAW_RECT
+
+
+IS_LAST_PIXEL:
+	# We're on the last pixel
+	beq $t2, $a2 END_DRAW_RECT
+	
+	# We're not on the last pixel, create a new row
+	bne $t2, $a2, NEW_ROW
+
+NEW_ROW:
+	# Reset the column count
+	li $t1, 0
+
+	# Restore the column offset
+	lw $t0, displayAddress
+	add $t0, $t0, $a3
+	
+	# We now shift it down however many rows on the row counter, which is address + 128 * t2
+	# t3 stores the vertical shift amount.
+	li $t3, 128
+	mul $t3, $t3, $t2
+	
+	add $t0, $t0, $t3
+	
+	
+	# Increment the row counter
+	addi $t2, $t2, 1
+	
+	# Call DRAW_RECT
+	j DRAW_RECT
+
+
+END_DRAW_RECT:
+	# reset the counters
+	li $t1, 0 
+	li $t2, 0
+	
+	# reset the display address to its base address
+	lw $t0, displayAddress
+	jr $ra
+	
 main:
 	# Call DRAW_RECT
 	# Usage:
@@ -21,9 +76,9 @@ main:
 	# Let's draw a green rectangle for the safe zone
 	
 	# Setting our variables based on the specifications above
-	li $a0, 0x00f00
-	li $a1, 2
-	li $a2, 2
+	li $a0, 0x00ff00
+	li $a1, 1
+	li $a2, 1
 	li $a3, 4
 	
 	# Register t1 is a counter variable for the current column
@@ -31,47 +86,7 @@ main:
 	li $t1, 0
 	li $t2, 1
 	
-	j DRAW_RECT
-
-END_DRAW_RECT:
-	# reset the counters
-	li $t1, 0 
-	li $t2, 0
+	# Shift Offset
+	add $t0, $t0, $a3
 	
-	# reset the display address to its base address
-	lw $t0, displayAddress
-
-IS_LAST_PIXEL:
-	# We're on the last pixel
-	beq $t2, $a2 END_DRAW_RECT
-	
-	# We're not on the last pixel, create a new row
-	bneq $t2, $a2, NEW_ROW
-
-NEW_ROW:
-	# Reset the column count
-	li $t1, 0
-	
-	# Increment the row counter
-	addi $t5, $t5, 1
-	
-	# Restore the column offset and shift it down by one row on the displayAddress (+128)
-	lw $t0, displayAddress
-	addi $t0, $t0, 128
-	add $t0, $t0, $a3 
-	
-	# Call DRAW_RECT
-	j DRAW_RECT
-
-DRAW_RECT:
-	# Increment the column position (initially from 0 if new column or brand new call), and draw a pixel
-	addi $t1, $t1, 1
-	sw $a0, 0($t0)
-	
-	# Shift the displayAddress pivot
-	addi $t0, $t0, 4
-	
-	# Check if we're done drawing the rectangle
-	beq $t1, $a1, IS_LAST_PIXEL
-	
-	
+	jal DRAW_RECT
