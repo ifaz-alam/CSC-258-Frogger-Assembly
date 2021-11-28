1313ef0713e3fd18efb4eaaac58c5b1cb027efdc
Ifaz A <ifazalam33@gmail.com>
2021-11-27 20:02:29 -0500

    Update frogger.asm

--- frogger.asm
+++ frogger.asm
@@ -23,7 +23,7 @@ li $t3, 0x0000ff # $t3 stores the blue colour code
 addi $t4, $zero, 0
 addi $t5, $zero, 0
 
-addi $t0, $t0, 4092
+addi $t0, $t0, 0
 
 #sw $t2, 0($t0)
 
@@ -51,45 +51,61 @@ DRAW_LAST_PIXEL: sw $a0, 0($t0)
 # DRAW THE REMAINING PIXEL BEFORE WRAPPING AROUND
 WRAP_AROUND: sw $a0, 0($t0)
 	addi $t0, $t0, -128
+	jr $ra # jump back
+	
+NEW_COLUMN: 
+	# decrement row counter by 1
+	addi $t5, $t5, -1
+	j DRAW_RECT
 
 DRAW_RECT:
 	# t4 = a1 = # columns 
 	add $t4, $zero, $a1
 	
+	# t5 = a2 = # row
+	add $t5, $zero, $a2
+	
 	# if t4 = 0: call END_DRAW_RECT with new row
-	beq $t4, $zero, DRAW_RECT
-	# else
+	beq $t4, $zero, NEW_COLUMN
 	
 	# draw pixel of colour $a0, then move to the right
 	sw $a0, 0($t0)
 	addi $t0, $t0, 4
 	
+	# Store t6 = a0 + 4
+	add $t6, $a0, 4
 	
-	# Store t5 = a0 + 4
-	add $t5, $a0, 4
-	
-	# Calcuate $t5 modulo 128, which is stored in register $hi
-	div $t5, 128
+	# Calcuate $t6 modulo 128, which is stored in register $hi
+	li $t7, 128
+	div $t6, $t7
 	
 	# if $t0 is an edge pixel, wrap around but before you do that add a pixel.
-	beq $hi, $zero, WRAP_AROUND
-	addi $t0, $t0, -128
+	mfhi $t8
+	beq $t8, $zero, WRAP_AROUND
 	
 	# t4 -= 1
 	addi $t4, $t4, -1
-
+	
+	# If we are done printing columns for 1 row, we go to the next
+	beq $t4, $zero, DRAW_RECT
+	
+	beq $t5, $zero, END_DRAW_RECT
 END_DRAW_RECT:
+	li $t4, 0 # reset the counter
+	lw $t0, displayAddress # reset the display address anchor
 	jr $ra
+	
 main:
 	# We want a draw rectangle function 
 	# Its paramters will be $a0 -> colour code, $a1 -> num column, $a2 -> num row, $a3 -> num column
 	li $a0, 0x00ff00 # store the green colour code
 	li $a1, 3
+	li $a2, 3
 	
-	# starting offset of displayAddress
-	addi $t0, $t0, 128
+	# set offset of displayAddress
+	addi $t0, $t0, 0
 	
-	#jal DRAW_RECTANGLE
+	jal DRAW_RECT
 	
 
 
