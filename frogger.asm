247f7975f3ad0d8407e3fd16a5717e4188002c56
Ifaz A <ifazalam33@gmail.com>
2021-11-27 19:44:51 -0500

    Update frogger.asm
    modular arithmetic wrap prototype

--- frogger.asm
+++ frogger.asm
@@ -21,12 +21,14 @@ li $t3, 0x0000ff # $t3 stores the blue colour code
 
 # The counter variable for number of pixels drawn in a row
 addi $t4, $zero, 0
+addi $t5, $zero, 0
 
-# t4 denotes the horizontal offset, a multiple of 32 renders the next row.
-# To get n rows worth of visuals, we need an upper bound of 32n - 1.
-# 7 rows worth of safe zone, is given by 32 * 9 - 1 = 223
+addi $t0, $t0, 4092
 
+#sw $t2, 0($t0)
 
+# To get the address of the n'th edge, we do 124 + 128(n - 1) = 128n - 128 + 124 = 128n - 4. 
+# So we can compute for the edge when $t0 + 4 is congruent to 0 modulo 128 
 j main
 
 DRAW_RECTANGLE:  beq $t4, $a1, DRAW_LAST_PIXEL
@@ -46,16 +48,48 @@ DRAW_LAST_PIXEL: sw $a0, 0($t0)
 		 lw $t0, displayAddress # reset the display address anchor
 		 jr $ra # jump back
 
+# DRAW THE REMAINING PIXEL BEFORE WRAPPING AROUND
+WRAP_AROUND: sw $a0, 0($t0)
+	addi $t0, $t0, -128
+
+DRAW_RECT:
+	# t4 = a1 = # columns 
+	add $t4, $zero, $a1
+	
+	# if t4 = 0: call END_DRAW_RECT with new row
+	beq $t4, $zero, DRAW_RECT
+	# else
+	
+	# draw pixel of colour $a0, then move to the right
+	sw $a0, 0($t0)
+	addi $t0, $t0, 4
+	
+	
+	# Store t5 = a0 + 4
+	add $t5, $a0, 4
+	
+	# Calcuate $t5 modulo 128, which is stored in register $hi
+	div $t5, 128
+	
+	# if $t0 is an edge pixel, wrap around but before you do that add a pixel.
+	beq $hi, $zero, WRAP_AROUND
+	addi $t0, $t0, -128
+	
+	# t4 -= 1
+	addi $t4, $t4, -1
+
+END_DRAW_RECT:
+	jr $ra
 main:
 	# We want a draw rectangle function 
-	# Its paramters will be $a0 -> colour code, $a1 -> num column, $a2 -> num row
+	# Its paramters will be $a0 -> colour code, $a1 -> num column, $a2 -> num row, $a3 -> num column
 	li $a0, 0x00ff00 # store the green colour code
-	li $a1, 0	
+	li $a1, 3
 	
 	# starting offset of displayAddress
 	addi $t0, $t0, 128
 	
-	jal DRAW_RECTANGLE
+	#jal DRAW_RECTANGLE
 	
 
 
