1ca872ab7e815a40087937a23c003706a46432ed
Ifaz A <ifazalam33@gmail.com>
2021-11-27 21:42:26 -0500

    Update frogger.asm
    can draw rectangle in middle of screen, but infinite looping bug occurs. need to fix it

--- frogger.asm
+++ frogger.asm
@@ -53,58 +53,80 @@ WRAP_AROUND: sw $a0, 0($t0)
 	addi $t0, $t0, -128
 	jr $ra # jump back
 	
-NEW_COLUMN: 
-	# decrement row counter by 1
-	addi $t5, $t5, -1
-	j DRAW_RECT
+NEW_ROW: 
+	# reset column traversed count
+	li $t4, 0
+	
+	beq $a2, $zero, END_DRAW_RECT
+	
+	# reset the column offset	
+	lw $t0, displayAddress
+	li $t1 128
+	add $t0, $t0, $a3
+	add $t0, $t0, $t1
+	
+	# increment row counter by 1
+	addi $t5, $t5, 1
+	
+	j DRAW_RECT_WHILE
 
 DRAW_RECT:
-	# t4 = a1 = # columns 
-	add $t4, $zero, $a1
+	# t4 is a counter variable counting the # of columns for a fixed row
+	li $t4, 0
 	
-	# t5 = a2 = # row
-	add $t5, $zero, $a2
+	# t5 is a counter variable counting the # of rows traversed
+	li $t5, 0
 	
-	# if t4 = 0: call END_DRAW_RECT with new row
-	beq $t4, $zero, NEW_COLUMN
+	beq $a1, $zero, END_DRAW_RECT
+	beq $a2, $zero, END_DRAW_RECT
 	
+	jal DRAW_RECT_WHILE
+	jr $ra
+	
+DRAW_RECT_WHILE:
 	# draw pixel of colour $a0, then move to the right
 	sw $a0, 0($t0)
 	addi $t0, $t0, 4
 	
 	# Store t6 = a0 + 4
-	add $t6, $a0, 4
+	addi $t6, $a0, 4
 	
 	# Calcuate $t6 modulo 128, which is stored in register $hi
-	li $t7, 128
-	div $t6, $t7
+	li $t7 128
+	#div $t6, $t7
 	
 	# if $t0 is an edge pixel, wrap around but before you do that add a pixel.
-	mfhi $t8
-	beq $t8, $zero, WRAP_AROUND
+	#mfhi $t7
 	
-	# t4 -= 1
-	addi $t4, $t4, -1
+	beq $t0, $t7, WRAP_AROUND
+	# beq $t7, $zero, WRAP_AROUND
+	#beq $t7, $zero, WRAP_AROUND
 	
-	# If we are done printing columns for 1 row, we go to the next
-	beq $t4, $zero, DRAW_RECT
+	# if we got all rows end the draw
+	beq $t5, $a2, END_DRAW_RECT
 	
-	beq $t5, $zero, END_DRAW_RECT
+	# t4 += 1
+	addi $t4, $t4, 1
+	
+	# if t4 = $a1: we go to a new row and recall the draw function (Since we have got all the columns for this row)
+	beq $t4, $a1, NEW_ROW
+	
+	j DRAW_RECT_WHILE
+	
 END_DRAW_RECT:
 	li $t4, 0 # reset the counter
 	lw $t0, displayAddress # reset the display address anchor
-	jr $ra
+	
 	
 main:
 	# We want a draw rectangle function 
-	# Its paramters will be $a0 -> colour code, $a1 -> num column, $a2 -> num row, $a3 -> num column
+	# Its paramters will be $a0 -> colour code, $a1 -> num column, $a2 -> num row, $a3 -> offset
 	li $a0, 0x00ff00 # store the green colour code
-	li $a1, 3
+	li $a1, 2
 	li $a2, 3
-	
+	li $a3, 28
 	# set offset of displayAddress
-	addi $t0, $t0, 0
-	
+	add $t0, $t0, $a3
 	jal DRAW_RECT
 	
 
